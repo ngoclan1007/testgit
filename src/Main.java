f6a0cd06074d7002e5fda0d4fbdd656aa2664dd6
ngoclan1007 <n2nlan@gmail.com>
2022-02-13 13:42:36 +0700

    Initial commit first

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+//        tổng
+        int resultSum = calculator.sum(8, 4);
+        System.out.println("tổng của hai số a, b là " + resultSum);
+    }
+}
---
+++ src/calculator.java
@@ -0,0 +1,6 @@
+public class calculator {
+    public static int sum(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+}
